5a575867416e2e40cdfd3ae340e1e7a80b9c1893
Lee GaEun <dlrkdms001@gmail.com>
2022-12-06 22:22:31 +0900

    KoSlang datafrmae upload

--- koslang/data_preprocessing/slang_processing.py
+++ koslang/data_preprocessing/slang_processing.py
@@ -1,1 +1,9 @@
 # Slang Processing
+import pandas as pd
+from pandas import Series, DataFrame
+import numpy as np
+
+#go-ring
+def ko_data(data_go):
+    data = pd.read_csv('koslang_last.txt', names=['prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation', 'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism', 'specific'], sep=',')
+    return data

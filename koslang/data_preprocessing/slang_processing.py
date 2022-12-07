f93aadc097129ccedcb7e697a345173e3839eaa8
chaeha617 <91950670+chaeha617@users.noreply.github.com>
2022-12-07 15:01:50 +0900

    Update slang_processing.py chaeha part

--- koslang/data_preprocessing/slang_processing.py
+++ koslang/data_preprocessing/slang_processing.py
@@ -5,3 +5,8 @@ import pandas as pd
 def ko_data():
     data = pd.read_csv('koslang_last.txt', names=['prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation', 'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism', 'specific'], sep=',')
     return data
+
+#chaeha
+def slang_TxtToDf():
+    Df = pd.read_csv('slang_text.txt', names=['','prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation', 'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism', 'specific'], sep=',',index_col=0)
+    return Df

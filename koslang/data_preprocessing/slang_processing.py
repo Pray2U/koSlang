eb9d09423941a12251a8a7754d8ab405b440358e
Choi JaeHoon <jhchoi09@naver.com>
2022-12-13 23:07:50 +0900

    Experiment time efficiency & performance utility

--- koslang/data_preprocessing/slang_processing.py
+++ koslang/data_preprocessing/slang_processing.py
@@ -3,10 +3,18 @@ import pandas as pd
 
 #go-ring
 def ko_data(path):
-    data = pd.read_csv(path,dtype={'': str,'prefix': int, 'suffix': int, 'interjection': int, 'fortis': int, 'idiom': int, 'abbreviation': int, 'long-phonetic': int, 'numNkor': int, 'engNkor': int, 'abstract': int, 'shape-shift': int, 'eroticism': int, 'specific': int}, names=['','prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation', 'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism', 'specific'], sep=',')
+    data = pd.read_csv(path,dtype={'': str,'prefix': int, 'suffix': int, 'interjection': int, 'fortis': int, 'idiom': int, 'abbreviation': int, 'long-phonetic': int, 'numNkor': int, 'engNkor': int, 'abstract': int, 'shape-shift': int, 'eroticism': int, 'specific': int}, names=['','prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation', 'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism', 'specific'], sep=',', index_col=0)
     return data
 
 #chaeha
 def slang_TxtToDf(path):
     Df = pd.read_csv(path,dtype={'': str,'prefix': int, 'suffix': int, 'interjection': int, 'fortis': int, 'idiom': int, 'abbreviation': int, 'long-phonetic': int, 'numNkor': int, 'engNkor': int, 'abstract': int, 'shape-shift': int, 'eroticism': int, 'specific': int}, names=['','prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation', 'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism', 'specific'], sep=',',index_col=0)
     return Df
+
+def createDataset(path):
+    dataset = pd.read_csv(path,
+                     dtype={'': str,'prefix': int, 'suffix': int, 'interjection': int, 'fortis': int, 'idiom': int, 'abbreviation': int, 'long-phonetic': int, 'numNkor': int, 'engNkor': int, 'abstract': int, 'shape-shift': int, 'eroticism': int, 'specific': int},
+                     names=['','prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation', 'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism', 'specific'],
+                     sep=',',
+                     index_col=0)
+    return dataset
--- koslang/modules.py
+++ koslang/modules.py
@@ -1,4 +1,5 @@
 # koSlang/koslang/modules.py
+import time
 from data_preprocessing import slang_processing
 from konlpy.tag import Kkma
 from eunjeon import Mecab
@@ -7,7 +8,6 @@ from konlpy.tag import Okt
 class koslang():
     def isSlang(self, sentence:str):
         '''
-
         :param sentence: Sentence with unspecified content.
         :return: Returns "True" if profanity is included, "False" otherwise.
 
@@ -23,29 +23,44 @@ class koslang():
             - okt.phrases()
         3. Check the test cases divided according to each case belong to the DataFrame one by one.
         4. If at least one case is included, we return "True" value, otherwise return "False" value.
-
-        ▼ Write your Code ▼
         '''
         kkma = Kkma()
         okt = Okt()
         mecab = Mecab()
 
-        slang_df = slang_processing.slang_TxtToDf('./data_preprocessing/slang_text.txt')
+        slang_df = slang_processing.createDataset('./data_preprocessing/dataset.txt')
 
+        # importingStartTime = time.time()
+        # kkmaStartTime = time.time()
         mo = kkma.morphs(sentence)
+        # kkmaEndTime = time.time()
+        # print(f"kkma : {mo}")
+        # oktStartTime = time.time()
         phr = okt.phrases(sentence)
-        noun = mecab.nouns(sentence)
-
+        # oktEndTime = time.time()
+        # print(f"okt : {phr}")
+        # mecabStartTime = time.time()
+        noun = mecab.morphs(sentence)
+        # mecabEndTime = time.time()
+        # print(f"mecab : {noun}")
+        # importingEndTime = time.time()
+        # print(f"All of Importing and Processing : {importingEndTime - importingStartTime:.5f}sec")
+        # print(f"Kkma Processing : {kkmaEndTime - kkmaStartTime:.5f}sec")
+        # print(f"Okt Processing : {oktEndTime - oktStartTime:.5f}sec")
+        # print(f"Mecab Processing : {mecabEndTime - mecabStartTime:.5f}sec")
+        #
+        # travelsaryStartTime = time.time()
         for slang in slang_df.index:
             if slang in mo or slang in noun or slang in phr:
                 return True
-
+        # travelsaryEndTime = time.time()
+        # print(f"All of Dataset Travelsary : {travelsaryEndTime - travelsaryStartTime:.5f}sec")
         return False
 
 
 if __name__ == "__main__":
     koslang = koslang()
-    exp = input("Input test sentence")
+    exp = input("Input test sentence : ")
     print(koslang.isSlang(exp))
 
 

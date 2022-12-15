7babb060e2d9ab72ad73c819e41fb5c32db636d2
Choi JaeHoon <jhchoi09@naver.com>
2022-12-15 22:59:31 +0900

    Result of Issue #5, Edit to core module

--- koslang/modules.py
+++ koslang/modules.py
@@ -24,37 +24,14 @@ class koslang():
         3. Check the test cases divided according to each case belong to the DataFrame one by one.
         4. If at least one case is included, we return "True" value, otherwise return "False" value.
         '''
-        kkma = Kkma()
-        okt = Okt()
         mecab = Mecab()
 
         slang_df = slang_processing.createDataset('./data_preprocessing/dataset.txt')
 
-        # importingStartTime = time.time()
-        # kkmaStartTime = time.time()
-        mo = kkma.morphs(sentence)
-        # kkmaEndTime = time.time()
-        # print(f"kkma : {mo}")
-        # oktStartTime = time.time()
-        phr = okt.phrases(sentence)
-        # oktEndTime = time.time()
-        # print(f"okt : {phr}")
-        # mecabStartTime = time.time()
         noun = mecab.morphs(sentence)
-        # mecabEndTime = time.time()
-        # print(f"mecab : {noun}")
-        # importingEndTime = time.time()
-        # print(f"All of Importing and Processing : {importingEndTime - importingStartTime:.5f}sec")
-        # print(f"Kkma Processing : {kkmaEndTime - kkmaStartTime:.5f}sec")
-        # print(f"Okt Processing : {oktEndTime - oktStartTime:.5f}sec")
-        # print(f"Mecab Processing : {mecabEndTime - mecabStartTime:.5f}sec")
-        #
-        # travelsaryStartTime = time.time()
         for slang in slang_df.index:
-            if slang in mo or slang in noun or slang in phr:
+            if slang in noun:
                 return True
-        # travelsaryEndTime = time.time()
-        # print(f"All of Dataset Travelsary : {travelsaryEndTime - travelsaryStartTime:.5f}sec")
         return False
 
 

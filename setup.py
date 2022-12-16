e6f9b39385f5000c6faff10aff2afeb55292fb84
Choi JaeHoon <jhchoi09@naver.com>
2022-12-16 21:00:34 +0900

    upload v1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='koslang',#module 이름
-    version='1.0.0',
+    version='1.0.1',
     description='Can detect slang in Korean.',
     author='koSlang',
     author_email='diadiahun0902@gmail.com',
@@ -11,7 +11,7 @@ setup(
     python_requires='>=3.10',
     long_description_content_type="text/markdown",
     license='MIT',
-    py_modules=['modules'], #업로드할 module
-    install_requires=["eunjeon"], #module 필요한 다른 module
+    py_modules=['koslang'], #업로드할 module
+    install_requires=["eunjeon","pandas"], #module 필요한 다른 module
     packages=['koslang'] #업로드할 module이 있는 폴더
 )

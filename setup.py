f860aaa434f6c280637682f80189d8acd7e79224
Choi JaeHoon <jhchoi09@naver.com>
2022-12-16 20:30:41 +0900

    Complete and Deploy pypi.org v1.0.0

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 # Pip_Package_Practice/setup.py
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-    setup(
-        name='koslang',#module 이름
-        version='1.0.0',
-        description='Can detect slang in Korean.',
-        author='koSlang',
-        author_email='diadiahun0902@gmail.com',
-        long_description=long_description,
-        python_requires='>=3.10',
-        long_description_content_type="text/markdown",
-        license='MIT',
-        py_modules=['modules'], #업로드할 module
-        install_requires=["eunjeon"], #module 필요한 다른 module
-        packages=['koslang'] #업로드할 module이 있는 폴더
-    )
+setup(
+    name='koslang',#module 이름
+    version='1.0.0',
+    description='Can detect slang in Korean.',
+    author='koSlang',
+    author_email='diadiahun0902@gmail.com',
+    long_description="",
+    python_requires='>=3.10',
+    long_description_content_type="text/markdown",
+    license='MIT',
+    py_modules=['modules'], #업로드할 module
+    install_requires=["eunjeon"], #module 필요한 다른 module
+    packages=['koslang'] #업로드할 module이 있는 폴더
+)

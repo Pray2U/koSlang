872b9cadf70f87b2c4091b37b9f4a4905aae6b40
Choi JaeHoon <jhchoi09@naver.com>
2022-12-16 21:28:38 +0900

    Update v1.0.3

--- koslang/koslang.py
+++ koslang/koslang.py
@@ -2,41 +2,36 @@
 from eunjeon import Mecab
 import pandas as pd
 
-class koslang():
-    def isSlang(self, sentence:str):
-        '''
-        :param sentence: Sentence with unspecified content.
-        :return: Returns "True" if profanity is included, "False" otherwise.
+def isSlang(sentence:str):
+    '''
+    :param sentence: Sentence with unspecified content.
+    :return: Returns "True" if profanity is included, "False" otherwise.
 
-        kkma.morphs()
-        mecab.nouns()
-        okt.phrases()
+    mecab.nouns()
 
-        Explain to our algorithm.
-        1. Import dataframe
-        2. Parameter Sentence is divided into test cases using the following three functions.
-            - kkma.morphs()
-            - mecab.nouns()
-            - okt.phrases()
-        3. Check the test cases divided according to each case belong to the DataFrame one by one.
-        4. If at least one case is included, we return "True" value, otherwise return "False" value.
-        '''
-        dataset = pd.read_csv("./data_preprocessing/dataset.txt",
-                              dtype={''        : str, 'prefix': int, 'suffix': int, 'interjection': int, 'fortis': int,
-                                     'idiom'   : int, 'abbreviation': int, 'long-phonetic': int, 'numNkor': int,
-                                     'engNkor' : int, 'abstract': int, 'shape-shift': int, 'eroticism': int,
-                                     'specific': int},
-                              names=['', 'prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation',
-                                     'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism',
-                                     'specific'],
-                              sep=',',
-                              index_col=0)
-        mecab = Mecab()
+    Explain to our algorithm.
+    1. Import dataframe
+    2. Parameter Sentence is divided into test cases using the following three functions.
+        - kkma.morphs()
+        - mecab.nouns()
+        - okt.phrases()
+    3. Check the test cases divided according to each case belong to the DataFrame one by one.
+    4. If at least one case is included, we return "True" value, otherwise return "False" value.
+    '''
+    dataset = pd.read_csv("./dataset.txt",
+                          dtype={''        : str, 'prefix': int, 'suffix': int, 'interjection': int, 'fortis': int,
+                                 'idiom'   : int, 'abbreviation': int, 'long-phonetic': int, 'numNkor': int,
+                                 'engNkor' : int, 'abstract': int, 'shape-shift': int, 'eroticism': int,
+                                 'specific': int},
+                          names=['', 'prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation',
+                                 'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism',
+                                 'specific'],
+                          sep=',',
+                          index_col=0)
+    mecab = Mecab()
 
-        # slang_df = slang_processing.createDataset('./data_preprocessing/dataset.txt')
-
-        noun = mecab.morphs(sentence)
-        for slang in dataset.index:
-            if slang in noun:
-                return True
-        return False
+    noun = mecab.morphs(sentence)
+    for slang in dataset.index:
+        if slang in noun:
+            return True
+    return False
--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='koslang',#module 이름
-    version='1.0.1',
+    version='1.0.3',
     description='Can detect slang in Korean.',
     author='koSlang',
     author_email='diadiahun0902@gmail.com',
     long_description="",
-    python_requires='>=3.10',
-    long_description_content_type="text/markdown",
+    python_requires='>=3.8',
     license='MIT',
     py_modules=['koslang'], #업로드할 module
     install_requires=["eunjeon","pandas"], #module 필요한 다른 module

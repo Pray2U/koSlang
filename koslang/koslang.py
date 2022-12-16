ed7e36e77b4bbd315befac92f0a6aaeb404b2932
Choi JaeHoon <jhchoi09@naver.com>
2022-12-17 00:26:19 +0900

    update v1.0.6

--- koslang/koslang.py
+++ koslang/koslang.py
@@ -19,23 +19,14 @@ def isSlang(sentence:str):
     4. If at least one case is included, we return "True" value, otherwise return "False" value.
     '''
 
-    dataset = pd.read_csv("./dataset.txt",
-                          dtype={''        : str, 'prefix': int, 'suffix': int, 'interjection': int, 'fortis': int,
-                                 'idiom'   : int, 'abbreviation': int, 'long-phonetic': int, 'numNkor': int,
-                                 'engNkor' : int, 'abstract': int, 'shape-shift': int, 'eroticism': int,
-                                 'specific': int},
-                          names=['', 'prefix', 'suffix', 'interjection', 'fortis', 'idiom', 'abbreviation',
-                                 'long-phonetic', 'numNkor', 'engNkor', 'abstract', 'shape-shift', 'eroticism',
-                                 'specific'],
-                          sep=',',
-                          index_col=0)
+    dataset = pd.read_csv("data.csv", sep=',', encoding='cp949', index_col=0)
     mecab = Mecab()
-
     noun = mecab.morphs(sentence)
+
     for slang in dataset.index:
         if slang in noun:
             return True
     return False
 
 if __name__=="__main__":
-     print(isSlang("이게 왜 안되는데 씨발아"))
+     print(isSlang("이게 왜 안되는데"))
